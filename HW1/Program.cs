5677d64e8a626ff17e507044c8132db5d183951b
Artoria5 <90453502+Artoria5@users.noreply.github.com>
2025-09-09 20:32:10 +0800

    HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class PalindromeChecker
+{
+    static void Main()
+    {
+        List<string> originalList = new List<string>();
+        List<string> reversedList = new List<string>();
+        List<string> resultList = new List<string>();
+
+        while (true)
+        {
+            Console.Write("\nEnter string: ");
+            string input = Console.ReadLine();
+
+            if (input == "*")
+            {
+                break; 
+            }
+
+            string lowerInput = input.ToLower();
+
+            string reversed = "";
+            for (int i = lowerInput.Length - 1; i >= 0; i--)
+            {
+                reversed += lowerInput[i];
+            }
+
+            bool isPalindrome = (lowerInput == reversed);
+
+            originalList.Add(input);
+            reversedList.Add(reversed);
+            resultList.Add(isPalindrome ? "Yes" : "No");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("{0,-20} {1,-20} {2,-12}", "Original String", "Reversed String", "Palindrome?");
+        for (int i = 0; i < originalList.Count; i++)
+        {
+            Console.WriteLine("{0,-20} {1,-20} {2,-12}",
+                originalList[i], reversedList[i], resultList[i]);
+        }
+
+        
+    }
+}
