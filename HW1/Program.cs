7118eed160f0449aa694f3fe61df6c7896f5106e
Artoria5 <90453502+Artoria5@users.noreply.github.com>
2025-09-09 20:32:10 +0800

    HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
-class PalindromeChecker
+class Program
 {
     static void Main()
     {
-        List<string> originalList = new List<string>();
-        List<string> reversedList = new List<string>();
-        List<string> resultList = new List<string>();
-
         while (true)
         {
             Console.Write("\nEnter string: ");
@@ -16,32 +11,28 @@ class PalindromeChecker
 
             if (input == "*")
             {
-                break; 
+                Console.WriteLine("\n//End execution");
+                break;
             }
 
             string lowerInput = input.ToLower();
-
             string reversed = "";
+
             for (int i = lowerInput.Length - 1; i >= 0; i--)
             {
                 reversed += lowerInput[i];
             }
 
-            bool isPalindrome = (lowerInput == reversed);
+          
+            string palindrome = (lowerInput == reversed) ? "Yes" : "No";
 
-            originalList.Add(input);
-            reversedList.Add(reversed);
-            resultList.Add(isPalindrome ? "Yes" : "No");
+          
+            Console.WriteLine("\n{0,-25}{1,-25}{2,-15}",
+                "Original String", "Reversed String", "Palindrome?");
+
+          
+            Console.WriteLine("{0,-25}{1,-25}{2,-15}",
+                input, reversed, palindrome);
         }
-
-        Console.WriteLine();
-        Console.WriteLine("{0,-20} {1,-20} {2,-12}", "Original String", "Reversed String", "Palindrome?");
-        for (int i = 0; i < originalList.Count; i++)
-        {
-            Console.WriteLine("{0,-20} {1,-20} {2,-12}",
-                originalList[i], reversedList[i], resultList[i]);
-        }
-
-        
     }
 }
