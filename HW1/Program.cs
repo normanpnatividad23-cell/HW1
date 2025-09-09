6dcfab89b579b3ca48b1236e7dd4289802f6d663
Artoria5 <90453502+Artoria5@users.noreply.github.com>
2025-09-09 20:32:10 +0800

    HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -11,26 +11,26 @@ class Program
 
             if (input == "*")
             {
-                Console.WriteLine("\n//End execution");
-                break;
+                break; 
             }
 
             string lowerInput = input.ToLower();
             string reversed = "";
 
+            
             for (int i = lowerInput.Length - 1; i >= 0; i--)
             {
                 reversed += lowerInput[i];
             }
 
-          
+   
             string palindrome = (lowerInput == reversed) ? "Yes" : "No";
 
           
             Console.WriteLine("\n{0,-25}{1,-25}{2,-15}",
                 "Original String", "Reversed String", "Palindrome?");
 
-          
+           
             Console.WriteLine("{0,-25}{1,-25}{2,-15}",
                 input, reversed, palindrome);
         }
