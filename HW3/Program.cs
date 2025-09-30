0d441514ffd76527f216eab82e9e1a62d3c81613
Artoria5 <artoriaracela@gmail.com>
2025-09-30 20:26:07 +0800

    HW3

---
+++ HW3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Dictionary<char, int> GetCharFrequency(string input)
+    {
+        Dictionary<char, int> frequency = new Dictionary<char, int>();
+        foreach (char c in input)
+        {
+            if (c == ' ' || c == ',') continue;
+            if (frequency.ContainsKey(c))
+                frequency[c]++;
+            else
+                frequency[c] = 1;
+        }
+        return frequency;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Enter string: ");
+        string input = Console.ReadLine();
+        string[] parts = input.Split(',');
+
+        foreach (string part in parts)
+        {
+            Dictionary<char, int> result = GetCharFrequency(part.Trim());
+            foreach (var kvp in result)
+            {
+                Console.Write(kvp.Key + "=" + kvp.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
