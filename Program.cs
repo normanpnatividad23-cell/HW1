bcbb3e39d87e73cdbe9cc5c3a288b2be69a602bc
Artoria5 <artoriaracela@gmail.com>
2025-09-30 20:44:23 +0800

    hw3

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Dictionary<char, int> CountChars(string text)
+    {
+        var map = new Dictionary<char, int>();
+        foreach (char c in text.Replace(" ", "").Replace(",", ""))
+        {
+            if (map.ContainsKey(c))
+                map[c]++;
+            else
+                map[c] = 1;
+        }
+        return map;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter string: ");
+        string line = Console.ReadLine();
+        string[] items = line.Split(',');
+
+        foreach (string item in items)
+        {
+            var result = CountChars(item.Trim());
+            int i = 0;
+            foreach (var pair in result)
+            {
+                Console.Write(pair.Key + "=" + pair.Value);
+                if (++i < result.Count) Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
